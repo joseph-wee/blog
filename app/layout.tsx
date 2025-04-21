62cbbabeee84abe8a26a0a9dcde325386c54e698
Wee Yosup <weeyosup@Weeui-iMac.local>
2025-04-22 06:27:21 +0900

    fix: 홈 페이지 메타 적용안되는거 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./prism.css";
 
 import { useEffect, useState } from "react";
 
-import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -173,17 +172,6 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Head>
-        <meta name="apple-mobile-web-app-title" content="Joseph.log" />
-        <meta name="og:title" content="Joseph.log" />
-        <meta
-          name="og:description"
-          content="개발관련 기록을 남기는 공간입니다."
-        />
-        <meta name="og:type" content="website" />
-        <meta name="og:image" content="https://josephlog.info/img_OG.png" />
-        {/* 여기에 다른 메타 태그도 추가 가능 */}
-      </Head>
       <body
         className={`flex  pl-[280px] md1000:pl-0 bg-white ${
           !mount && "opacity-0"
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+export const generateMetadata = () => {
+  return {
+    title: "Joseph.log",
+    description: "개발관련 기록을 남기는 공간입니다.",
+    openGraph: {
+      title: "Joseph.log",
+      description: "개발관련 기록을 남기는 공간입니다.",
+      type: "website",
+      images: [{ url: "https://josephlog.info/img_OG.png" }],
+    },
+  };
+};
+
 export default function Home() {
   return <div>안녕하세요. 개발관련 메모를 남기는 공간입니다.</div>;
 }
